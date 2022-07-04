4a856ea9e70772a541c44f0991ed453ea50db55b
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-07-04 21:09:40 +0530

    add map function and its tests

--- testing framework/index.js
+++ testing framework/index.js
@@ -7,4 +7,12 @@ module.exports = {
       fn(arr[idx], idx);
     }
   },
+  map(arr, fn) {
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      result.push(fn(arr[i], i));
+    }
+    return result;
+  },
 };
--- testing framework/index.test.js
+++ testing framework/index.test.js
@@ -1,4 +1,4 @@
-const { forEach } = require('./index.js');
+const { forEach, map } = require('./index.js');
 
 let sum = 0;
 
@@ -6,6 +6,22 @@ forEach([1, 2, 3], (value, i) => {
   sum += value;
 });
 
-if (sum !== 7) {
+let result = map([1, 2, 3], (value) => {
+  return value * 2;
+});
+
+if (sum !== 6) {
   throw new Error('Expected sum to equal 6');
+}
+
+if (result[0] !== 2) {
+  throw new Error(`Expected result[0] to equal 2 but received ${result[0]}`);
+}
+
+if (result[1] !== 4) {
+  throw new Error('Expected result[1] to equal 4');
+}
+
+if (result[2] !== 6) {
+  throw new Error('Expected result[2] to equal 6');
 }
