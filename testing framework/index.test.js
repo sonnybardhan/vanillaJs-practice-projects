9f8fa31ae643b2e4f6f5e8720378e259e443814e
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-07-04 21:23:32 +0530

    improve testings library

--- testing framework/index.test.js
+++ testing framework/index.test.js
@@ -1,27 +1,40 @@
 const { forEach, map } = require('./index.js');
 
-let sum = 0;
+const test = (desc, fn) => {
+  console.log(`----`, desc);
+  try {
+    fn();
+  } catch (err) {
+    console.log('Whoops! ', err.message);
+  }
+};
 
-forEach([1, 2, 3], (value, i) => {
-  sum += value;
+test('Tests for forEach', () => {
+  let sum = 0;
+
+  forEach([1, 2, 3], (value, i) => {
+    sum += value;
+  });
+
+  if (sum !== 6) {
+    throw new Error('Expected sum to equal 6');
+  }
 });
 
-let result = map([1, 2, 3], (value) => {
-  return value * 2;
+test('Tests for map', () => {
+  let result = map([1, 2, 3], (value) => {
+    return value * 2;
+  });
+
+  if (result[0] !== 2) {
+    throw new Error(`Expected result[0] to equal 2 but received ${result[0]}`);
+  }
+
+  if (result[1] !== 4) {
+    throw new Error('Expected result[1] to equal 4');
+  }
+
+  if (result[2] !== 6) {
+    throw new Error('Expected result[2] to equal 6');
+  }
 });
-
-if (sum !== 6) {
-  throw new Error('Expected sum to equal 6');
-}
-
-if (result[0] !== 2) {
-  throw new Error(`Expected result[0] to equal 2 but received ${result[0]}`);
-}
-
-if (result[1] !== 4) {
-  throw new Error('Expected result[1] to equal 4');
-}
-
-if (result[2] !== 6) {
-  throw new Error('Expected result[2] to equal 6');
-}
