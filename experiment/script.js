6650445f21771719cc0c29e81af8d1092491dc4a
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-08-06 02:16:53 +0530

    complete div resize across x axis (right)

--- experiment/script.js
+++ experiment/script.js
@@ -1,76 +1,51 @@
 const body = document.querySelector('body');
 const box2 = document.querySelector('#box2');
-let clickedDown = false;
 
-const box2Dimensions = {
-  x: null,
-  y: null,
-  width: null,
-  height: null,
-};
+let stretch = false;
 
 box2.addEventListener('mousedown', function (e) {
   const { top, left, right, bottom } = e.target.getBoundingClientRect();
   const mouseX = e.clientX;
-  const mouseY = e.clientY;
-  const borderWidth = 10;
+  // const mouseY = e.clientY;
 
-  if (mouseX > right - borderWidth) {
-    clickedDown = true;
+  if (mouseX > right - 10) {
+    stretch = true;
+  } else {
+    stretch = false;
   }
-
-  // const newX = mouseX - left;
-
-  // if (left + newX > left + 25) {
-  //   console.log('entering');
-  //   e.target.style.width = newX + 'px';
-  // }
-});
-
-box2.addEventListener('mouseleave', function (e) {
-  clickedDown = false;
 });
 
 box2.addEventListener('mousemove', function (e) {
-  const { top, left, right, bottom } = e.target.getBoundingClientRect();
+  const { right } = e.target.getBoundingClientRect();
   const mouseX = e.clientX;
-  const mouseY = e.clientY;
-  const borderWidth = 10;
 
-  // if (mouseX > right - borderWidth) {
-  //   this.classList.add('add-right-border');
-  // } else {
-  //   this.classList.remove('add-right-border');
-  // }
-
-  if (mouseX > right - borderWidth && clickedDown) {
-    if (mouseX > left + 50) {
-      this.style.width = mouseX + 'px';
-    }
+  if (mouseX > right - 10) {
+    this.classList.add('add-right-border');
+  } else {
+    this.classList.remove('add-right-border');
   }
-
-  // if (mouseX > right - borderWidth || mouseX < left + borderWidth) {
-  //   e.target.style.cursor = 'col-resize';
-  // } else if (mouseY < top + borderWidth || mouseY > bottom - borderWidth) {
-  //   e.target.style.cursor = 'row-resize';
-  // } else {
-  //   e.target.style.cursor = '';
-  // }
 });
 
-box2.addEventListener('mouseup', function (e) {
-  const { top, left, right, bottom } = e.target.getBoundingClientRect();
-  const mouseX = e.clientX;
-  const mouseY = e.clientY;
-  clickedDown = false;
-  // const newX = mouseX - left;
-
-  // if (left + newX > left + 25) {
-  //   console.log('entering');
-  //   e.target.style.width = newX + 'px';
-  // }
+box2.addEventListener('mouseleave', function (e) {
+  this.classList.remove('add-right-border');
 });
 
 body.addEventListener('mousemove', function (e) {
-  console.log('moving');
+  const mouseX = e.clientX;
+
+  // const { x, y, left, right, top, bottom, width } =
+  const { left } = box2.getBoundingClientRect();
+
+  if (stretch) {
+    box2.style.width = `${mouseX - left}px`;
+    box2.classList.add('add-right-border');
+  }
+});
+
+body.addEventListener('mouseup', function (e) {
+  if (stretch) {
+    stretch = false;
+    // this.classList.add('add-right-border');
+    box2.classList.remove('add-right-border');
+  }
 });
