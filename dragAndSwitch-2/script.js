8f9d005fcbb727154743b264aff378f54a3bd072
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-08-06 15:48:21 +0530

    add smooth drag functionality

--- dragAndSwitch-2/script.js
+++ dragAndSwitch-2/script.js
@@ -1,6 +1,11 @@
 const body = document.querySelector('body');
 const box = document.getElementById('box1');
 const resizers = document.querySelectorAll('.resizer');
+const dragHandle = document.querySelector('.draghandle');
+
+dragHandle.setAttribute('draggable', true);
+
+body.addEventListener('dragover', (e) => e.preventDefault());
 
 resizers.forEach((resizer) => {
   resizer.addEventListener('mousedown', onMouseDownResizer);
@@ -37,7 +42,7 @@ function onMouseDownResizer(e) {
 
 function onMouseUp(e) {
   body.removeEventListener('mousemove', onMouseMove);
-  console.log('mouseup triggered');
+  body.removeEventListener('mouseup', onMouseUp);
   axis = '';
 }
 
@@ -45,13 +50,45 @@ function onMouseMove(e) {
   const dx = e.clientX - initialPosition.x;
   const dy = e.clientY - initialPosition.y;
 
-  console.log('axis: ', axis);
-
   if (axis === 'x') {
     box.style.width = `${dimensions.w + dx}px`;
   } else {
     box.style.height = `${dimensions.h + dy}px`;
   }
+}
 
-  // console.log('dx: ', dx, 'dy: ', dy);
+//drag functions
+
+const initialBoxPosition = {
+  x: 0,
+  y: 0,
+};
+
+const boxClick = {
+  x: 0,
+  y: 0,
+};
+
+dragHandle.addEventListener('dragstart', onDragStart);
+
+dragHandle.addEventListener('dragend', onDragEnd);
+
+function onDragStart(e) {
+  const { x, y, top, left } = e.target.offsetParent.getBoundingClientRect();
+  initialBoxPosition.x = x;
+  initialBoxPosition.y = y;
+
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  boxClick.x = mouseX - left;
+  boxClick.y = mouseY - top;
+}
+
+function onDragEnd(e) {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  box.style.left = mouseX - boxClick.x + 'px';
+  box.style.top = mouseY - boxClick.y + 'px';
 }
