3fa26706d8ea6c7c3aeb6310d75ef98db2a63822
abhirup bardhan <bardhan.abhirup@gmail.com>
2022-10-11 01:35:23 +0530

    attempt filter polyfill

--- polyfills/filter.js
+++ polyfills/filter.js
@@ -1,11 +1,30 @@
-Array.prototype.myFilter = function (cb) {
-  const temp = [];
-  this.forEach((element, idx) => {
-    if (cb(element, idx, this)) {
-      temp.push(element);
+// Array.prototype.myFilter = function (cb) {
+//   const temp = [];
+//   this.forEach((element, idx) => {
+//     if (cb(element, idx, this)) {
+//       temp.push(element);
+//     }
+//   });
+//   return temp;
+// };
+
+Array.prototype.myFilter = function (fn) {
+  if (!Array.isArray(this)) return;
+
+  let result = [];
+  // for (let i = 0; i < this.length; i++) {
+  //   if (fn(this[i], i, this)) {
+  //     result.push(this[i]);
+  //   }
+  // }
+
+  this.forEach((item, idx) => {
+    if (fn(item, idx, this)) {
+      result.push(item);
     }
   });
-  return temp;
+
+  return result;
 };
 
 const arr = [1, 2, 3, 4];
